2a7a0d8fa00750bdf41bf3b1f08cbdbf854a9542
Dylan Marais <dylanmarais100@gmail.com>
2018-06-13 15:48:11 +0200

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('booking','bookingsController');
 
-Route::resource('booking','bookingsController');
 
 Route::resource('food','FoodController');
 
